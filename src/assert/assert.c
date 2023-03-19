616350c56d06cb4afb159e2d51bf6e87e4ad2286
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2023-03-19 08:17:13 -0400

    fix compiler whining

--- src/assert/assert.c
+++ src/assert/assert.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-[[gnu::weak]] [[noreturn]] void __assert_fail(const char *expr,
-											  const char *file,
-											  unsigned int line,
-											  const char *function)
+[[gnu::weak]] void __assert_fail(const char *expr, const char *file,
+								 unsigned int line, const char *function)
 {
 	printf("%s:%u: assertion in function %s failed expression %s\n", file, line,
 		   function, expr);
