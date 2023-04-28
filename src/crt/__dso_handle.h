c399b1004a4a6c76a08cd2bcc0b985f3908a2bee
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2023-04-27 20:54:55 -0400

    Revert "Merge remote-tracking branch 'origin/trunk' into trunk"
    This reverts commit 84b6fcd033da31e64f3a72bd39a2559ce33f6c34.

--- src/crt/__dso_handle.h
+++ src/crt/__dso_handle.h
@@ -1,6 +1,6 @@
 #ifndef __DSO_HANDLE_H__
 #define __DSO_HANDLE_H__
 
-[[gnu::visibility("hidden")]] void *__dso_handle;
+__attribute__((visibility("hidden"))) void *__dso_handle;
 
 #endif /* __DSO_HANDLE_H__ */
